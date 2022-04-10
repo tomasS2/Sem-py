f9b7608e8682b24676c6f29b99b2be5e6135f290
tomasS2 <ilidtoto@gmail.com>
2022-04-10 15:41:53 -0300

    modifico el 10

--- Practicas/Practica 2/pr2ej10.py
+++ Practicas/Practica 2/pr2ej10.py
@@ -7,29 +7,31 @@
 # • Calcular el promedio de las notas totales e informar que alumnos obtuvieron menos que el
 # promedio.
 
+archivo = open ("eval1.txt","r")
+notas1 = archivo.read().split(",")
+archivo.close()
+notas1 = list(map(int,notas1))  #genero una lista de enteros haciendo uso de la funcion map() e int().
 
-# with open ("nombres_1.txt") as file_object:
-#     nombres = file_object.read()
-with open ("eval1.txt") as file_object:
-    eval1 = file_object.read()
-with open ("eval2.txt") as file_object:
-    eval2 = file_object.read()
+archivo = open ("eval2.txt","r")
+notas2 = archivo.read().split(",")
+archivo.close()
+notas2 = list(map(int,notas2))  #genero una lista de enteros haciendo uso de la funcion map() e int().
+
+archivo = open ("nombres_1.txt","r")
+nombres = archivo.read().split("\n")
+archivo.close()
 
 
-
-def generarEstructuraEstudiantes (nombres,eval1,eval2):
+def generarEstructuraEstudiantes (nombres,notas_suma):
     """calcula el promedio de las notas y 
     genera diccionario que tiene como clave los nombre de 
     los alumnos y como valores la suma de sus notas.  """
 
-    diccio_alum = {}
-    sumador_prom = 0
-    prom = 0
+    info_alum = {}
     for i in range(len(nombres)):
-        diccio_alum[nombres[i]]=eval1[i]+eval2[i]
-        sumador_prom = sumador_prom + eval1[i]+eval2[i]
-    prom = sumador_prom/(len(eval1)+len(eval2))
-    return diccio_alum, prom
+        info_alum[nombres[i]] = notas_suma[i]
+    prom = sum(notas_suma) / (len(notas_suma))
+    return info_alum, prom
 
 
 def informarPromedioInferior(diccio_alum,prom):
@@ -40,14 +42,14 @@ def informarPromedioInferior(diccio_alum,prom):
             print (f"alumno/a {elem} obtuvo una nota inferior al promedio")
 
 
+notas_suma = []
 
-#nombres = nombres.split(",")#problemas con los \n
-nombres = 'Agustin','Alan','Andrés','Ariadna','Bautista','CAROLINA','CESAR','David','Diego','Dolores','DYLAN','ELIANA','Emanuel','Fabián','Facundo','Facundo','FEDERICO','FEDERICO','GONZALO','Gregorio','Ignacio','Jonathan','Jonathan','Jorge','JOSE','JUAN','Juan','Juan','Julian','Julieta','LAUTARO','Leonel','LUIS','Luis','Marcos','María','MATEO','Matias','Nicolás','NICOLAS','Noelia','Pablo','Priscila','TOMAS','Tomás','Ulises','Yanina'
-eval1 = eval1.split(",")
-eval1 = list(map(int,eval1))
-eval2 = eval2.split(",")
-eval2 = list(map(int,eval2))
+for i in range(len(notas1)):
+    notas_suma.append(notas1[i] + notas2[i])
+
+for i,nom in enumerate(nombres):    #recorro la lista de nombres y reemplazo las comas por nada.
+    nombres[i] = nom.replace(",","")
 
 
-diccio_alum,prom = generarEstructuraEstudiantes(nombres,eval1,eval2)
+diccio_alum,prom = generarEstructuraEstudiantes(nombres,notas_suma)
 informarPromedioInferior(diccio_alum,prom)
