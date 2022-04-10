e40f0c6cad37b77630e6168b244371bc0f5acbc1
tomasS2 <ilidtoto@gmail.com>
2022-04-10 15:30:57 -0300

    subo resolucion 9

--- Practicas/Practica 2/pr2ej9.py
+++ Practicas/Practica 2/pr2ej9.py
@@ -23,13 +23,144 @@
 # Nota: Defina al menos una función en el código (si hay mas mejor) y documente las mismas con
 # docstring que es lo que hacen.
 
-# [
-# '-*-*-',
-# '--*--',
-# '----*',
-# '*----',
-# ]
 
-from gettext import find
 
-mapa=['-*-*-','--*--','----*','*----',]
+mapa = ['-*-*-','--*--','----*','*----']
+
+res = [0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]
+
+for x, fila in enumerate(mapa):
+    for y, elem in enumerate(fila):
+
+        if (elem == "-"):
+
+            #caso que este el guion entre espacios.
+            if (x > 0) and (y > 0) and (x < len(mapa)-1) and (y < len(fila)-1):
+                if (mapa[x+1][y+1] == "*"):
+                    res[x][y] = res[x][y] + 1
+
+                if (mapa[x-1][y-1] == "*"):
+                    res[x][y] = res[x][y] + 1
+
+                if (mapa[x][y+1] == "*"):
+                    res[x][y] = res[x][y] + 1
+
+                if (mapa[x][y-1] == "*"):
+                    res[x][y] = res[x][y] + 1
+
+                if (mapa[x+1][y] == "*"):
+                    res[x][y] = res[x][y] + 1
+
+                if (mapa[x-1][y] == "*"):
+                    res[x][y] = res[x][y] + 1
+
+                if (mapa[x-1][y+1] == "*"):
+                    res[x][y] = res[x][y] + 1
+
+                if (mapa[x+1][y-1] == "*"):
+                    res[x][y] = res[x][y] + 1
+
+
+            else:
+                #caso que el guion este en la pos 'x' 0
+                if (x == 0):
+                    # caso que el guion este en la pos 'x' 0 e 'y' no este en la pos 0 ni len.
+                    if (y > 0) and (y < len(fila)-1):
+                        if (mapa[x][y-1] == "*"):
+                            res[x][y] = res[x][y] + 1
+                        if (mapa[x+1][y-1] == "*"):
+                            res[x][y] = res[x][y] + 1                  
+                        if (mapa[x][y+1] == "*"):
+                            res[x][y] = res[x][y] + 1
+                        if (mapa[x+1][y] == "*"):
+                            res[x][y] = res[x][y] + 1
+                        if (mapa[x+1][y+1] == "*"):
+                            res[x][y] = res[x][y] + 1
+                    else:
+                        # caso que el guion este en la pos 'x' 0 e 'y' este en la pos 0
+                        if (y == 0):
+                            if (mapa[x][y+1] == "*"):
+                                res[x][y] = res[x][y] + 1
+                            if (mapa[x+1][y] == "*"):
+                                res[x][y] = res[x][y] + 1
+                            if (mapa[x+1][y+1] == "*"):
+                                res[x][y] = res[x][y] + 1
+                        else:
+                             # caso que el guion este en la pos 'x' 0 e 'y' este en la pos len
+                            if (y == len(fila)-1):
+                                if (mapa[x+1][y] == "*"):
+                                    res[x][y] = res[x][y] + 1
+                                if (mapa[x][y-1] == "*"):
+                                    res[x][y] = res[x][y] + 1
+                                if (mapa[x+1][y-1] == "*"):
+                                    res[x][y] = res[x][y] + 1
+
+
+                else:
+                    #caso que el guion este en la pos 'x' len.
+                    if (x == len(mapa)-1):
+                        # caso que el guion este en la pos 'x' len e 'y' no este en la pos 0 ni len.
+                        if (y > 0) and (y < len(fila)-1):
+                            if (mapa[x][y-1] == "*"):
+                                res[x][y] = res[x][y] + 1
+                            if (mapa[x-1][y-1] == "*"):
+                                res[x][y] = res[x][y] + 1
+                            if (mapa[x][y+1] == "*"):
+                                res[x][y] = res[x][y] + 1
+                            if (mapa[x-1][y+1] == "*"):
+                                res[x][y] = res[x][y] + 1
+
+                        else:
+                            # caso que el guion este en la pos 'x' len e 'y'  este en la pos 0.
+                            if (y == 0):
+                                if (mapa[x-1][y] == "*"):
+                                    res[x][y] = res[x][y] + 1
+                                if (mapa[x][y+1] == "*"):
+                                    res[x][y] = res[x][y] + 1
+                                if (mapa[x-1][y+1] == "*"):
+                                    res[x][y] = res[x][y] + 1
+                            else:
+                                # caso que el guion este en la pos 'x' len e 'y' este en la pos len
+                                if (y == len(fila)-1):
+                                    if (mapa[x-1][y] == "*"):
+                                        res[x][y] = res[x][y] + 1
+                                    if (mapa[x][y-1] == "*"):
+                                        res[x][y] = res[x][y] + 1
+                                    if (mapa[x-1][y-1] == "*"):
+                                        res[x][y] = res[x][y] + 1
+
+
+                    ## CASO Y
+                    else:
+                    #caso que el guion este en la pos 'y' 0
+                        if (y == 0):
+                    # caso que el guion este en la pos 'y' 0 e 'x' no este en la pos 0 ni len.
+                            if (x > 0) and (x < len(mapa)-1):
+                                if (mapa[x+1][y] == "*"):
+                                    res[x][y] = res[x][y] + 1
+                                if (mapa[x+1][y+1] == "*"):
+                                    res[x][y] = res[x][y] + 1
+                                if (mapa[x-1][y+1] == "*"):
+                                    res[x][y] = res[x][y] + 1
+                                if (mapa[x-1][y] == "*"):
+                                    res[x][y] = res[x][y] + 1
+                        else:
+                            if (y == len(fila)-1):
+                                if (mapa[x+1][y] == "*"):
+                                    res[x][y] = res[x][y] + 1
+                                if (mapa[x+1][y-1] == "*"):
+                                    res[x][y] = res[x][y] + 1
+                                if (mapa[x-1][y-1] == "*"):
+                                    res[x][y] = res[x][y] + 1
+                                if (mapa[x-1][y] == "*"):
+                                    res[x][y] = res[x][y] + 1
+        else:
+            res[x][y] = "*"
+# for x, fila in enumerate(mapa):
+#     for y, elem in enumerate(fila):
+#         if x == 0:
+#             res[x][y] =  res[x][y] +1
+
+
+
+print (res)
